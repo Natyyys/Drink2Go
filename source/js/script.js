0e79a725f28b6848669eda3a6e4eff9372269957
Наталья Моисеева <mns-fw@bk.ru>
2023-10-10 00:06:39 +0300

    feat: стилизует header на 320 с js

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,18 @@
 /* в этот файл добавляет скрипты*/
+const CLASS_NAV_OPEN = 'main-nav--opened';
+const CLASS_NAV_JS = 'main-nav--js';
+
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (!(navMain && navToggle)) {
+    return;
+  }
+
+  navMain.classList.add(CLASS_NAV_JS);
+
+  navToggle.addEventListener('click', () => {
+    navMain.classList.toggle(CLASS_NAV_OPEN);
+  });
+});
