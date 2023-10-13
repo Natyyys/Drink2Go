7cd03fc09275d3d974c80b7d98f360acc045daad
Наталья Моисеева <mns-fw@bk.ru>
2023-10-13 23:56:44 +0300

    pixel perfect 768, корректирует header

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,20 +1,18 @@
 /* в этот файл добавляет скрипты*/
 import './slider.js'
 
-const CLASS_NAV_OPEN = 'main-nav--opened';
-const CLASS_NAV_JS = 'main-nav--js';
+let navMain = document.querySelector(".main-nav");
+let navToggle = document.querySelector(".main-nav__toggle");
 
-const navMain = document.querySelector('.main-nav');
-const navToggle = document.querySelector('.main-nav__toggle');
+navMain.classList.remove("main-nav--nojs");
+navMain.classList.add("main-nav--closed");
 
-document.addEventListener('DOMContentLoaded', () => {
-  if (!(navMain && navToggle)) {
-    return;
+navToggle.addEventListener("click", function () {
+  if (navMain.classList.contains("main-nav--closed")) {
+    navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
+  } else {
+    navMain.classList.add("main-nav--closed");
+    navMain.classList.remove("main-nav--opened");
   }
-
-  navMain.classList.add(CLASS_NAV_JS);
-
-  navToggle.addEventListener('click', () => {
-    navMain.classList.toggle(CLASS_NAV_OPEN);
-  });
 });
